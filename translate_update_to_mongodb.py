dcec24ba935e33bb21ead933c8aac783116e4d88
An Panharith <an.panharith111@gmail.com>
2024-12-02 16:18:37 +0300

    add translation threshold char count

--- translate_update_to_mongodb.py
+++ translate_update_to_mongodb.py
@@ -5,6 +5,8 @@ import zlib  # For compression
 
 FILE_PATH = './dataset/official_Phishing_Email.csv'
 TARGET_LANGUAGE = 'fr'
+HAS_THRESHOLD_LIMIT = True
+TRANSLATION_THRESHOLD_CHAR = 10_000
 
 # Connect to MongoDB
 client = MongoClient("mongodb://admin:admin@localhost:27017/")  # Adjust your URI accordingly
@@ -23,7 +25,7 @@ def loading_dataset():
         print(f"Error loading dataset: {e}")
         return None
 
-def update_translate_to_mongo(email_text):
+def update_translate_to_mongo(email_text, current_count):
     result = {}
     try:
         # Try to store uncompressed data for small emails
@@ -55,21 +57,57 @@ def update_translate_to_mongo(email_text):
         # Find the document with the specified hash that doesn't have TARGET_LANGUAGE
         document = collection.find_one(query)
         if document:
-            # Assuming translate_text() returns a dictionary like {"language.fr": "translated text"}
+
+            # Validate on Threshold char
+            next_count = current_count + len(document['email_text'])
+            if HAS_THRESHOLD_LIMIT and next_count > TRANSLATION_THRESHOLD_CHAR:
+                return -1 # signal to the caller function to stop the executiion
+
+            translated_text = translate_text(TARGET_LANGUAGE, document['email_text'])
+            if len(email_bytes) > threshold_size:
+                translated_text = zlib.compress(
+                    bytes(translated_text, 'utf-8'))
             new_value = {
                 "$set": {
-                    TARGET_LANGUAGE: translate_text(TARGET_LANGUAGE, document['email_text'])
+                    TARGET_LANGUAGE: translated_text
                 }
             }
             # Perform the update on the found document
             collection.update_one({"_id": document["_id"]}, new_value)
-            print(f"Document updated with {TARGET_LANGUAGE}")
+            print(f"Document updated with target language {TARGET_LANGUAGE}")
+            return len(document['email_text'])
         else:
             print("No document found that needs updating.")
+            return 0
     except errors.DuplicateKeyError:
         print(f"Document with hash {hash_value} already exists.")
+        return 0
     except Exception as e:
         print(f"An error occurred: {e}")
+        return 0
+
+# def google_translate_text(target, text):
+#     """Translates text into the target language.
+#     Target must be an ISO 639-1 language code.
+#     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+#     """
+#     return
+#     import six
+#     from google.cloud import translate_v2 as translate
+#
+#     translate_client = translate.Client()
+#
+#     if isinstance(text, six.binary_type):
+#         text = text.decode("utf-8")
+#
+#     # Text can also be a sequence of strings, in which case this method
+#     # will return a sequence of results for each text.
+#     result = translate_client.translate(text, target_language=target)
+#
+#     # print(u"Text: {}".format(result["input"]))
+#     # print(u"Translation: {}".format(result["translatedText"]))
+#     # print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+#     return result["translatedText"]
 
 def translate_text(lang, text):
     translated_text = 'ttt'
@@ -81,6 +119,7 @@ def iterate_each_row(df, start_index, end_index):
         print("Invalid index range specified.")
         return
 
+    count = 0
     # Iterate over the specified range
     for index, row in df.iloc[start_index:end_index].iterrows():
         # Ensure essential fields are non-empty strings
@@ -90,9 +129,19 @@ def iterate_each_row(df, start_index, end_index):
         if not email_text or not email_type:
             continue  # Skip rows with missing values
 
-        update_translate_to_mongo(email_text)
+        next_count = update_translate_to_mongo(email_text, count)
+        if next_count <0: # stop the execution when it's a negative count
+            break
+        count += next_count
+
+    with open("translate_log.txt", "a") as f:
+        log = f"total translated characters = {count}. in language {TARGET_LANGUAGE}\n\n"
+        print(log)
+        f.write(log)
 
 if __name__ == '__main__':
+    # tr = google_translate_text('fr', 'Cambodia')
+    # print(tr)
     print("Loading dataset")
     df = loading_dataset()
     if df is not None:
