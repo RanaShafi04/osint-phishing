96cbf98230687f594b25eda722ae26fb03be4c24
An Panharith <an.panharith111@gmail.com>
2024-12-07 12:48:43 +0300

    fix extracting domain

--- utilities.py
+++ utilities.py
@@ -6,15 +6,60 @@ from mongodb_conn import mongo_collection, convert_objectid_to_str
 
 URL_PATTERN = r'\b(?:https?://|www\.)\S+\b'
 EMAIL_PATTERN = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-DOMAIN_EXTRACTION_PATTERN = r'^(?:https?:\/\/)?(?:www\.)?([^\/\n]+)'
+# DOMAIN_EXTRACTION_PATTERN = r'^(?:https?:\/\/)?(?:www\.)?([^\/\n]+)'
+DOMAIN_EXTRACTION_PATTERN = r"([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})"
 
 # Configuration
 RETRIES = 3
 TIMEOUT = 5  # seconds
 
+# Generic TLDs (gTLDs)
+GENERIC_TLDS = [
+    ".com", ".org", ".net", ".info", ".biz", ".name", ".pro", ".coop", ".aero", ".museum", ".int",
+    ".edu", ".gov", ".mil", ".jobs", ".mobi", ".travel", ".tel", ".post", ".asia", ".cat", ".xxx",
+    ".club", ".online", ".site", ".store", ".tech", ".app", ".blog", ".shop", ".cloud", ".dev",
+    ".art", ".company", ".design", ".group", ".software", ".agency", ".media", ".network"
+]
+
+# Country-code TLDs (ccTLDs)
+COUNTRY_TLDS = [
+    ".ac", ".ad", ".ae", ".af", ".ag", ".ai", ".al", ".am", ".ao", ".aq", ".ar", ".as", ".at", ".au", ".aw",
+    ".ax", ".az", ".ba", ".bb", ".bd", ".be", ".bf", ".bg", ".bh", ".bi", ".bj", ".bm", ".bn", ".bo", ".bq",
+    ".br", ".bs", ".bt", ".bv", ".bw", ".by", ".bz", ".ca", ".cc", ".cd", ".cf", ".cg", ".ch", ".ci", ".ck",
+    ".cl", ".cm", ".cn", ".co", ".cr", ".cu", ".cv", ".cw", ".cx", ".cy", ".cz", ".de", ".dj", ".dk", ".dm",
+    ".do", ".dz", ".ec", ".ee", ".eg", ".eh", ".er", ".es", ".et", ".eu", ".fi", ".fj", ".fk", ".fm", ".fo",
+    ".fr", ".ga", ".gb", ".gd", ".ge", ".gf", ".gg", ".gh", ".gi", ".gl", ".gm", ".gn", ".gp", ".gq", ".gr",
+    ".gs", ".gt", ".gu", ".gw", ".gy", ".hk", ".hm", ".hn", ".hr", ".ht", ".hu", ".id", ".ie", ".il", ".im",
+    ".in", ".io", ".iq", ".ir", ".is", ".it", ".je", ".jm", ".jo", ".jp", ".ke", ".kg", ".kh", ".ki", ".km",
+    ".kn", ".kp", ".kr", ".kw", ".ky", ".kz", ".la", ".lb", ".lc", ".li", ".lk", ".lr", ".ls", ".lt", ".lu",
+    ".lv", ".ly", ".ma", ".mc", ".md", ".me", ".mg", ".mh", ".mk", ".ml", ".mm", ".mn", ".mo", ".mp", ".mq",
+    ".mr", ".ms", ".mt", ".mu", ".mv", ".mw", ".mx", ".my", ".mz", ".na", ".nc", ".ne", ".nf", ".ng", ".ni",
+    ".nl", ".no", ".np", ".nr", ".nu", ".nz", ".om", ".pa", ".pe", ".pf", ".pg", ".ph", ".pk", ".pl", ".pm",
+    ".pn", ".pr", ".ps", ".pt", ".pw", ".py", ".qa", ".re", ".ro", ".rs", ".ru", ".rw", ".sa", ".sb", ".sc",
+    ".sd", ".se", ".sg", ".sh", ".si", ".sj", ".sk", ".sl", ".sm", ".sn", ".so", ".sr", ".ss", ".st", ".sv",
+    ".sx", ".sy", ".sz", ".tc", ".td", ".tf", ".tg", ".th", ".tj", ".tk", ".tl", ".tm", ".tn", ".to", ".tr",
+    ".tt", ".tv", ".tw", ".tz", ".ua", ".ug", ".uk", ".us", ".uy", ".uz", ".va", ".vc", ".ve", ".vg", ".vi",
+    ".vn", ".vu", ".wf", ".ws", ".ye", ".yt", ".za", ".zm", ".zw"
+]
+
+# Combine into one list
+VALID_TLDS = GENERIC_TLDS + COUNTRY_TLDS
+
+def is_valid_domain(domain):
+    """Validate domain ends with a valid TLD."""
+    return any(domain.endswith(tld) for tld in VALID_TLDS)
+
 def extract_domain(url):
+    """Extract and clean domain from a URL."""
+    # Regular expression to extract domain
     match = re.search(DOMAIN_EXTRACTION_PATTERN, url)
-    return match.group(1) if match else None
+    if match:
+        domain = match.group(1)
+        # Remove trailing invalid characters
+        domain = domain.strip(")").strip(",:\"")
+        # Validate domain
+        return domain if is_valid_domain(domain) else None
+    return None
 
 def extract_domain_link_email(email_text):
     result = {}
